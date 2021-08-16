d847cc9dbae166c31c12fddb3d1671d62ff63e88
Admin <Admin@Maitrayee>
2021-08-16 15:53:26 +0530

    First commit

---
+++ newproject/src/main/java/newproject/salesclass.java
@@ -0,0 +1,15 @@
+package newproject;
+
+public class salesclass {
+
+	public void display()
+	{
+		int [] arr= {23,45,65,78,2};
+		for(int num:arr)
+		{
+			System.out.println(num);
+		}
+	}
+	
+	
+}
