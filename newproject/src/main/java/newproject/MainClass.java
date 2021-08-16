856ec76fad8377bb178f0929107509538f32f230
Admin <Admin@Maitrayee>
2021-08-16 15:45:43 +0530

    First Commit

---
+++ newproject/src/main/java/newproject/MainClass.java
@@ -0,0 +1,15 @@
+package newproject;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello good morning");
+		System.out.println("Good Afternoon");
+		System.out.println("Hi");
+		
+		
+
+	}
+
+}
