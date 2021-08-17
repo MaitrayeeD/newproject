cbf791027e51e971ef7db5cebfbe3aeb5a39b790
Admin <Admin@Maitrayee>
2021-08-17 11:00:34 +0530

    display method updated

--- newproject/src/main/java/newproject/salesclass.java
+++ newproject/src/main/java/newproject/salesclass.java
@@ -5,11 +5,15 @@ public class salesclass {
 	public void display()
 	{
 		int [] arr= {23,45,65,78,2};
-		for(int num:arr)
+		for(int i=0;i<arr.length;i++)
 		{
-			System.out.println(num);
+			System.out.println(arr[i]);
+	
 		}
+		
+		System.out.println("Array size"+arr.length);
 	}
+	
 	public void print()
 	{
 		System.out.println("Print all array members");
